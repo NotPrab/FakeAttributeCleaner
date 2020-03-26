03dfa9aa1a35a8db0e46eaa9aeba5c90ecac35c0
Prab <54489997+NotPrab@users.noreply.github.com>
2020-03-26 14:25:14 +0700

    Source

---
+++ FakeAttributeCleaner/FakeAttribute.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using dnlib.DotNet;
+
+namespace FakeAttributeCleaner
+{
+    class FakeAttribute
+    {
+        public static void Execute(ModuleDefMD module)
+        {
+            int j = 0;
+            for (int i = 0; i < module.CustomAttributes.Count; i++)
+            {
+				CustomAttribute attribute = module.CustomAttributes[i];
+                bool flag = attribute == null;
+                if (!flag)
+                {
+					TypeDef type = attribute.AttributeType.ResolveTypeDef();
+					bool flag2 = type == null;
+                    if (!flag2)
+                    {
+                        bool flag3 = type.FullName == "ConfusedByAttribute";
+                        if (flag3)
+                        {
+                            module.Types.Remove(type);
+                            module.CustomAttributes.Remove(attribute);
+                            j++;
+                        }
+						bool flag4 = type.FullName == "ZYXDNGuarder";
+						if (flag4)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag5 = type.FullName == "YanoAttribute";
+						if (flag5)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag6 = type.FullName == "Xenocode.Client.Attributes.AssemblyAttributes.ProcessedByXenocode";
+						if (flag6)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag7 = type.FullName == "SmartAssembly.Attributes.PoweredByAttribute";
+						if (flag7)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag8 = type.FullName == "SecureTeam.Attributes.ObfuscatedByAgileDotNetAttribute";
+						if (flag8)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag9 = type.FullName == "SecureTeam.Attributes.ObfuscatedByCliSecureAttribute";
+						if (flag9)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag10 = type.FullName == "ObfuscatedByGoliath";
+						if (flag10)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag11 = type.FullName == "NineRays.Obfuscator.SoftwareWatermarkAttribute";
+						if (flag11)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag12 = type.FullName == "NineRays.Obfuscator.Evaluation";
+						if (flag12)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag13 = type.FullName == "EMyPID_8234_";
+						if (flag13)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag14 = type.FullName == "DotfuscatorAttribute";
+						if (flag14)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag15 = type.FullName == "CryptoObfuscator.ProtectedWithCryptoObfuscatorAttribute";
+						if (flag15)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag16 = type.FullName == "BabelObfuscatorAttribute";
+						if (flag16)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag17 = type.FullName == ".NETGuard";
+						if (flag17)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag18 = type.FullName == "OrangeHeapAttribute";
+						if (flag18)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+						bool flag19 = type.FullName == "<ObfuscatedByDotNetPatcher>";
+						if (flag19)
+						{
+							module.Types.Remove(type);
+							module.CustomAttributes.Remove(attribute);
+							j++;
+						}
+					}
+                }
+            }
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("[+] Removed Fake Attribute {0}", j);
+			Console.ForegroundColor = ConsoleColor.Yellow;
+		}
+    }
+}
---
+++ FakeAttributeCleaner/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using dnlib.DotNet;
+using dnlib.DotNet.Writer;
+
+namespace FakeAttributeCleaner
+{
+    class Program
+    {
+        public static bool veryVerbose = false;
+        public static string Asmpath;
+        public static ModuleDefMD module;
+        public static Assembly asm;
+        static void Main(string[] args)
+        {
+            Console.Title = "FakeAttribute Cleaner";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string directory = args[0];
+
+            try
+            {
+                Program.module = ModuleDefMD.Load(directory);
+                Program.asm = Assembly.LoadFrom(directory);
+                Program.Asmpath = directory;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Not a .NET assembly");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            AssemblyDef assembly = AssemblyDef.Load(directory);
+            try { FakeAttribute.Execute(module); }
+            catch (Exception e) { Console.WriteLine($"Error while trying to remove FakeAttribute ." + e); }
+
+            string text = Path.GetDirectoryName(directory);
+            if (!text.EndsWith("\\"))
+            {
+                text += "\\";
+            }
+            string filename = string.Format("{0}{1}_Rem{2}", text, Path.GetFileNameWithoutExtension(directory), Path.GetExtension(directory));
+            ModuleWriterOptions writerOptions = new ModuleWriterOptions(module);
+            writerOptions.MetaDataOptions.Flags |= MetaDataFlags.PreserveAll;
+            writerOptions.Logger = DummyLogger.NoThrowInstance;
+            NativeModuleWriterOptions NativewriterOptions = new NativeModuleWriterOptions(module);
+            NativewriterOptions.MetaDataOptions.Flags |= MetaDataFlags.PreserveAll;
+            NativewriterOptions.Logger = DummyLogger.NoThrowInstance;
+            if (module.IsILOnly) { module.Write(filename, writerOptions); } else { module.NativeWrite(filename, NativewriterOptions); }
+
+            Console.WriteLine($"File Saved at : {filename}");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
